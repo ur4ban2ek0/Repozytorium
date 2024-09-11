6c849ba74b52597fd4766502549b2e09e5503ace
ur4ban2ek0 <urbanekktt@gmail.com>
2024-09-11 12:03:21 +0200

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //tablica
+        //musi miec z gory okreslony rozmiar
+        // nie mozna go pozniej zmienic
+        //elementem tablicy moga byc typy proste i zlozone
+        //tablica z wartosciamy losowym od 1 do 100 6 elementow
+        int tablicaLosowa[]= new int[6];
+        for (int i = 0; i < tablicaLosowa.length; i++) {
+            tablicaLosowa[i]=(int) (Math.random()*100+1);
+
+        }
+        //wypisywanie elementow tablicy
+        
+        for (int element:tablicaLosowa) {
+            System.out.print(element+", ");
+        }
+              {
+            
+        }
+
     }
 }
